582b3195d1cfe1ac43563ebe4ce97ddd295b982f
Faustin <firama06@gmail.com>
2022-09-09 13:02:29 -0700

    tasj 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,23 @@
-:wq
+#include <stdio.h>
+
+/*
+ * main - ommitting 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
