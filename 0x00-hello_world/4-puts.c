5c18e92d91de76e3abbfc335d38aa7a458e40916
Faustin <firama06@gmail.com>
2022-09-11 10:48:20 -0700

    t4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,14 @@
+#include <stdio.h>
+
 /*
- *
  * * main - Printing in c
  *
- * *
- *
  * * Return: Always 0 (Correct)
- *
  */
 
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual
-
-			puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
