4c3affc2b6b29776f2f7d690768e4b73355c7084
Faustin <firama06@gmail.com>
2022-09-11 12:32:53 -0700

    t3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,6 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
-/*
+/**
  * main - lowercase and uppercase letters
  *
  * Return: 0
@@ -10,18 +8,14 @@
 int main (void)
 {
 	char c;
-
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
-
 	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
 	}
-
 	putchar('\n');
-
 	return (0);
 }
