912cf1c7b91db527fb9112dbb6508873b98ea20a
Faustin <firama06@gmail.com>
2022-09-11 12:50:00 -0700

    t5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#insert <stdio.h>
+
+/**
+ * main - prints numbers 0 to 9
+ *
+ * Description: using the main function to print
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return 0;
+}
