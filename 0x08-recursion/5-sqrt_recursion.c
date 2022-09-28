15aa0baced857d385c59d0703719ace230ecd108
Faustin <firama06@gmail.com>
2022-09-28 13:44:38 -0700

    t5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * sqrt2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: 0
+ */
+
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
