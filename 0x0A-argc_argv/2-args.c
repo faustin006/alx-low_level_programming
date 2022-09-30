bbae885d58610c58d0f9b96ee52509a42db71b52
Faustin <firama06@gmail.com>
2022-09-30 12:23:43 -0700

    t2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - prints arguments a program receives
+ *
+ * argc: argument number
+ * argv: argument array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n;
+
+	for (n = 0; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+	return (0);
+}
