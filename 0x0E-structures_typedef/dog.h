b91b70526174de2b0281f1cbda40168f206a4c50
Faustin <firama06@gmail.com>
2022-10-10 07:31:18 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef STRTYP
+#define STRTYP
+
+/**
+ * struct dog - structure of a dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	chat *owner;
+};
+
+typedef struct dog dog_t;
+
+void inti_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name,  float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*STRTYP*/
