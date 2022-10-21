b4e5952136d82ca03f30a56e4e81fd2b16f80f37
Faustin <firama06@gmail.com>
2022-10-21 10:48:48 -0700

    t4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_lists - frees a list
+ * @head: head of the linked list
+ * Return: 0
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while ((current_node = head) != NULL)
+	{
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
