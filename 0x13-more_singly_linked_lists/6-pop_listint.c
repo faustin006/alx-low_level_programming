3cdaa55a4967d7897d5c352313b4c22ef1bedc5e
Faustin <firama06@gmail.com>
2022-10-25 05:29:37 -0700

    t6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes tje head of a listint_t lit
+ * @head: pointer to the head address
+ *
+ * Return: data of the head node
+ * 	Or if linked list is empty 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int ret;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	ret = (*head)->net;
+
+	free(tmp);
+
+	return (ret);
+}
